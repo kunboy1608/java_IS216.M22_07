ca4d0203c3036e9ede0a7533901958d54c853fdb
kunboy1608 <kunboy1608@gmail.com>
2022-03-30 21:11:41 +0700

    Templete Code

---
+++ src/com/controller/KhachHangController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.controller;
+
+/**
+ *
+ * @author kunbo
+ */
+public class KhachHangController {
+    /**
+     *  Nho ghi chu cac kieu 
+     *  Controller xu li cac chuc nang nhu ben duoi (ham duoi chi la vi du thoi)    
+     */
+    public void ThemKhachHang(){
+        
+    }
+}
---
+++ src/com/models/DataContext.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.models;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author kunbo
+ */
+public class DataContext {
+    // Tuong tu voi cac model khac
+    private LinkedList<KhachHangModel> KhachHangs;
+    
+}
