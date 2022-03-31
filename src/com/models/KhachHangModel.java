8d3a6f4d0be30da96ee06527e8727f9bba95beb8
kunboy1608 <kunboy1608@gmail.com>
2022-03-31 21:51:32 +0700

    Phu-Add Model
    KhachHangModel

--- src/com/models/KhachHangModel.java
+++ src/com/models/KhachHangModel.java
@@ -4,10 +4,50 @@
  */
 package com.models;
 
+import java.util.Date;
+
 /**
  *
  * @author kunbo
  */
 public class KhachHangModel {
+    private String MaNV;
+    private Date NgayVaoLam;
+    private String CCD;
+    
+    public KhachHangModel(){
+        
+    }
+
+    public KhachHangModel(String MaNV, Date NgayVaoLam, String CCD) {
+        this.MaNV = MaNV;
+        this.NgayVaoLam = NgayVaoLam;
+        this.CCD = CCD;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public Date getNgayVaoLam() {
+        return NgayVaoLam;
+    }
+
+    public void setNgayVaoLam(Date NgayVaoLam) {
+        this.NgayVaoLam = NgayVaoLam;
+    }
+
+    public String getCCD() {
+        return CCD;
+    }
+
+    public void setCCD(String CCD) {
+        this.CCD = CCD;
+    }
+    
     
 }
