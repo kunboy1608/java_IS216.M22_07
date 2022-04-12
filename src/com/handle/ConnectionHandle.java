4827fa8c4d54d00d1bc7501107d9ba009694aa7c
kunboy1608 <kunboy1608@gmail.com>
2022-04-12 21:40:15 +0700

    Phu - Handle
    Add Configuaration Loader
    Add ConnectionHandle

--- src/com/handle/ConnectionHandle.java
+++ src/com/handle/ConnectionHandle.java
@@ -4,16 +4,45 @@
  */
 package com.handle;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author kunbo
  */
 public class ConnectionHandle {
 
-    private static ConnectionHandle _instance = new ConnectionHandle();
+    private static final ConnectionHandle _instance = new ConnectionHandle();
+    private Connection con;
+    private String[] sInfo;
 
     private ConnectionHandle() {
+        sInfo = ConfigurationLoader.getInstance().getServerInfo();
+    }
 
+    public void Connect() {
+        String connectionUrl
+                = "jdbc:sqlserver://" + sInfo[0] + ":" + sInfo[2] + ";"
+                + "database=" + sInfo[1] + ";"
+                + "user=sa;"
+                + "password=Matkhau1;"
+                + "encrypt=true;"
+                + "trustServerCertificate=true;"
+                + "loginTimeout=5;";
+
+        try {
+            con = DriverManager.getConnection(connectionUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        if (con == null) {
+            Connect();
+        }
+        return con;
     }
 
     public static ConnectionHandle getInstance() {
--- src/com/models/DataContext.java
+++ src/com/models/DataContext.java
@@ -13,8 +13,20 @@ import java.util.LinkedList;
 public class DataContext {
 
     // Tuong tu voi cac model khac
+    private AccountModel account;
+
+    private LinkedList<CTHDModel> CTHDs;
+    private LinkedList<CTHDNCCModel> CTHDNCCs;
+    private LinkedList<ChiNhanhModel> ChiNhanhs;
+    private LinkedList<DoUongModel> DoUongs;
+    private LinkedList<GiamGiaModel> GiamGias;
+    private LinkedList<HoaDonKhachHangModel> HoaDonKhachHangs;
+    private LinkedList<HoaDonNCCModel> HoaDonNCCs;
     private LinkedList<KhachHangModel> KhachHangs;
-    
+    private LinkedList<NguyenLieuModel> NguyenLieus;
+    private LinkedList<NhaCungCapModel> NhaCungCaps;
+    private LinkedList<NhanVienModel> NhanViens;
+
     private final static DataContext _instance = new DataContext();
 
     private DataContext() {
@@ -23,5 +35,5 @@ public class DataContext {
     public static DataContext getInstance() {
         return _instance;
     }
-    
+
 }
