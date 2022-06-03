22b029a16baf0f5b6086abb52e781cfe459e44dc
hoangdp <kunboy1608@gmail.com>
2022-06-03 20:53:34 +0700

    Update NhanVienController.java
    Add method LayDuLieu

--- src/com/controller/NhanVienController.java
+++ src/com/controller/NhanVienController.java
@@ -5,10 +5,14 @@
 package com.controller;
 
 import com.handle.ConnectionHandle;
+import com.models.DataContext;
 import com.models.NhanVienModel;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,7 +32,7 @@ public class NhanVienController {
     }
 
     private NhanVienController() {
-
+        LayDuLieu();
     }
 
     public boolean ThemNhanVien(NhanVienModel nv) {
@@ -97,5 +101,30 @@ public class NhanVienController {
             return false;
         }
         return false;
+    }
+
+    public void LayDuLieu() {
+        try {
+            String sql = "Select *  FROM NHANVIEN";
+
+            Statement ps = ConnectionHandle.getInstance().getConnection().createStatement();
+            ResultSet rs = ps.executeQuery(sql);
+            LinkedList<NhanVienModel> list = new LinkedList<>();
+            NhanVienModel nv;
+            while (rs.next()) {
+                nv = new NhanVienModel();
+                nv.setMaNV(rs.getInt(1));
+                nv.setMaCN(rs.getInt(2));
+                nv.setTenNV(rs.getString(3));
+                nv.setGioiTinh(rs.getBoolean(4));
+                nv.setNgayVL(rs.getDate(5));
+                nv.setCCCD(rs.getString(6));
+                list.add(nv);
+            }
+            DataContext.getInstance().setNhanViens(list);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
